95b62688c0d551c61cc870596df6efaeafa77d31
Mahmoud Mohammed <mahmoudmohammed07@gmail.com>
2019-09-28 15:32:42 +0200

    Retrofit and parse RSS

---
+++ app/src/main/java/com/android/redditapp/FeedAPI.java
@@ -0,0 +1,13 @@
+package com.android.redditapp;
+
+import com.android.redditapp.model.Feed;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface FeedAPI {
+    String BASE_URL = "https://www.reddit.com/r/";
+
+    @GET("news/.rss")
+    Call<Feed> getFeed();
+}
