05d4e1dd3d55752f3163e608b393e04900bfed9e
Mahmoud Mohammed <mahmoudmohammed07@gmail.com>
2019-09-29 14:03:30 +0200

    Preparing data to be parsed

--- app/src/main/java/com/android/redditapp/MainActivity.java
+++ app/src/main/java/com/android/redditapp/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.android.redditapp.model.Feed;
+import com.android.redditapp.model.entry.Entry;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,8 +37,32 @@ public class MainActivity extends AppCompatActivity {
         call.enqueue(new Callback<Feed>() {
             @Override
             public void onResponse(Call<Feed> call, Response<Feed> response) {
-                Log.d(TAG, "onResponse: feed: " + response.body().getEntries());
+                //Log.d(TAG, "onResponse: feed: " + response.body().getEntries());
                 Log.d(TAG, "onResponse: Server Response: " + response.toString());
+
+                List<Entry> entries = response.body().getEntries();
+
+                Log.d(TAG, "onResponse: entries: " + response.body().getEntries());
+
+//                Log.d(TAG, "onResponse: author: " + entries.get(0).getAuthor().getName());
+//                Log.d(TAG, "onResponse: updated: " + entries.get(0).getUpdated());
+//                Log.d(TAG, "onResponse: title: " + entries.get(0).getTitle());
+                for (int i = 0; i < entries.size(); i++) {
+                    ExtractXML extractXML1 = new ExtractXML("<a href=", entries.get(0).getContent());
+                    List<String> postContent = extractXML1.start();
+
+                    ExtractXML extractXML2 = new ExtractXML("<img src=", entries.get(0).getContent());
+
+                    try {
+                        postContent.add(extractXML2.start().get(0));
+                    } catch (NullPointerException e) {
+                        postContent.add(null);
+                        Log.e(TAG, "onResponse: NullPointerException(thumbnail): " + e.getMessage());
+                    } catch (IndexOutOfBoundsException e) {
+                        postContent.add(null);
+                        Log.e(TAG, "onResponse: IndexOutOfBoundsException(thumbnail): " + e.getMessage());
+                    }
+                }
             }
 
             @Override
