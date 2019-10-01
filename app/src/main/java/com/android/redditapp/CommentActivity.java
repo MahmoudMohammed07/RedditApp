54773fbc257c413836380c862b358492c54379b9
Mahmoud Mohammed <mahmoudmohammed07@gmail.com>
2019-10-01 14:47:08 +0200

    Preparing to get comments from comments thread

--- app/src/main/java/com/android/redditapp/CommentActivity.java
+++ app/src/main/java/com/android/redditapp/CommentActivity.java
@@ -10,19 +10,32 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.redditapp.model.Feed;
+import com.android.redditapp.model.entry.Entry;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class CommentActivity extends AppCompatActivity {
 
     private static final String TAG = "CommentActivity";
+    private static final String BASE_URL = "https://www.reddit.com/r/";
 
     private static String postUrl;
     private static String postThumbnailUrl;
     private static String postTitle;
     private static String postAuthor;
     private static String postUpdated;
+
+    private String currentFeed;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +44,39 @@ public class CommentActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: Started...");
 
         initPost();
+
+        init();
+    }
+
+    private void init() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .build();
+
+        FeedAPI feedAPI = retrofit.create(FeedAPI.class);
+
+        Call<Feed> call = feedAPI.getFeed(currentFeed);
+
+        call.enqueue(new retrofit2.Callback<Feed>() {
+            @Override
+            public void onResponse(Call<Feed> call, Response<Feed> response) {
+                //Log.d(TAG, "onResponse: feed: " + response.body().getEntries());
+                Log.d(TAG, "onResponse: Server Response: " + response.toString());
+
+                List<Entry> entries = response.body().getEntries();
+
+                for (int i = 0; i < entries.size(); i++) {
+                    Log.d(TAG, "onResponse: entries: " + entries.get(i).toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Feed> call, Throwable t) {
+                Log.e(TAG, "onFailure: Unable to retrieve RSS: " + t.getMessage());
+                Toast.makeText(CommentActivity.this, "An Error Occurred", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initPost() {
@@ -53,6 +99,14 @@ public class CommentActivity extends AppCompatActivity {
         updated.setText(postUpdated);
 
         displayImage(postThumbnailUrl, thumbnail, progressBar);
+
+        try {
+            String[] splitURL = postUrl.split(BASE_URL);
+            currentFeed = splitURL[1];
+            Log.d(TAG, "initPost: currentFeed: " + currentFeed);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Log.d(TAG, "initPost: ArrayIndexOutOfBoundsException: " + e.getMessage());
+        }
     }
 
     private void displayImage(String imageURL, ImageView imageView, final ProgressBar progressBar) {
